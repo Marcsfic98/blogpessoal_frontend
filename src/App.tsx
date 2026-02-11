d3afa8013490f1bc9fb01e1721f8d72a2368aece
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-02-11 15:06:22 -0300

    navbar e footer criado usando tawilind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./assets/components/footer/Footer";
+import Navbar from "./assets/components/navbar/Navbar";
 import Home from "./assets/pages/home/Home";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
